1efcf8bbb6d121592fcb4c7397935ac5c2640174
verpenunes <106631829+verpenunes@users.noreply.github.com>
2023-02-22 14:53:35 +0100

    ricardo-nunes-pf.ma1.js

---
+++ ricardo-nunes-pf.ma1.js
@@ -0,0 +1,42 @@
+// question 1
+
+var number = 5;
+console.log(5);
+
+var boolean = true;
+console.log(boolean);
+
+var string = "String value";
+console.log(string);
+
+// question 2
+
+var firstName = "Ricardo";
+var lastName = "Nunes";
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+// question 3
+
+var amphibian = "frog";
+console.log(typeof amphibian);
+console.log("The type of frog is: ", typeof "frog");
+
+// question 4
+
+var orderHasShipped = false;
+
+if (orderHasShipped === true) {
+    console.log("The order shipped");
+}
+else { 
+    console.log("The order did not ship");
+}
+
+// question 5
+
+for (var i = 7; i <= 13; i++) {
+    console.log(i);
+}
